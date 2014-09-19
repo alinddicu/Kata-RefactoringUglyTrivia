7c9b804920a2c4926b6a1739fcd5b73f5f9834c7
alinddicu <alinddicu@yahoo.com>
2014-09-19 12:30:36 +0200

    Introducing the GameMaster

--- Kata-RefactoringUglyTrivia/Trivia/Game.cs
+++ Kata-RefactoringUglyTrivia/Trivia/Game.cs
@@ -13,74 +13,67 @@
         private readonly Action<string> _writeLine;
         private readonly GameAnnouncer _gameAnnouncer;
         private readonly QuestionPresentor _questionPresentor;
+        private readonly GameMaster _gameMaster;
 
-        private readonly List<Player> _players = new List<Player>();
         private readonly QuestionStack _questionStack = new QuestionStack();
-
-        int _currentPlayerIndex = 0;
-        private Player CurrentPlayer { get { return _players[_currentPlayerIndex]; } }
-
+        
         public Game(Action<string> writeLine)
         {
             _writeLine = writeLine;
             _gameAnnouncer = new GameAnnouncer(_writeLine);
             _questionPresentor = new QuestionPresentor(_gameAnnouncer, _questionStack);
+            _gameMaster = new GameMaster(_gameAnnouncer);
         }
 
         public bool AddPlayer(string playerName)
         {
-            var player = new Player(playerName);
-            _players.Add(player);
-            _gameAnnouncer.PlayerWasAdded(player);
-            _gameAnnouncer.PlayerPosition(_players.Count());
+            _gameMaster.AddPlayer(playerName);
 
             return true;
         }
 
         public int howManyPlayers()
         {
-            return _players.Count;
+            return _gameMaster.CountPlayers();
         }
 
         public void roll(int roll)
         {
-            _gameAnnouncer.CurrentPlayer(CurrentPlayer);
+            _gameAnnouncer.CurrentPlayer(_gameMaster.GetCurrentPlayer());
             _gameAnnouncer.CurrentRoll(roll);
 
-            if (CurrentPlayer.InPenaltyBox)
+            if (_gameMaster.GetCurrentPlayer().InPenaltyBox)
             {
                 if (roll % 2 != 0)
                 {
-                    CurrentPlayer.IsGettingOutOfPenaltyBox = true;
-
-                    _gameAnnouncer.CurrentPlayerGetsOutOfPenaltyBox(CurrentPlayer, true);
-
-                    _questionPresentor.AskNextQuestion(CurrentPlayer, roll);
+                    _gameMaster.GetCurrentPlayer().IsGettingOutOfPenaltyBox = true;
+                    _gameAnnouncer.CurrentPlayerGetsOutOfPenaltyBox(_gameMaster.GetCurrentPlayer(), true);
+                    _questionPresentor.AskNextQuestion(_gameMaster.GetCurrentPlayer(), roll);
                 }
                 else
                 {
-                    _gameAnnouncer.CurrentPlayerGetsOutOfPenaltyBox(CurrentPlayer, false);
-                    CurrentPlayer.IsGettingOutOfPenaltyBox = false;
+                    _gameAnnouncer.CurrentPlayerGetsOutOfPenaltyBox(_gameMaster.GetCurrentPlayer(), false);
+                    _gameMaster.GetCurrentPlayer().IsGettingOutOfPenaltyBox = false;
                 }
             }
             else
             {
-                _questionPresentor.AskNextQuestion(CurrentPlayer, roll);
+                _questionPresentor.AskNextQuestion(_gameMaster.GetCurrentPlayer(), roll);
             }
         }
 
         public bool wasCorrectlyAnswered()
         {
-            if (CurrentPlayer.InPenaltyBox)
+            if (_gameMaster.GetCurrentPlayer().InPenaltyBox)
             {
-                if (CurrentPlayer.IsGettingOutOfPenaltyBox)
+                if (_gameMaster.GetCurrentPlayer().IsGettingOutOfPenaltyBox)
                 {
                     return PlayRound("Answer was correct!!!!");
                 }
                 else
                 {
-                    _currentPlayerIndex++;
-                    StartNewRound();
+                    _gameMaster.SetNextPlayer();
+                    _gameMaster.StartNewRound();
 
                     return true;
                 }
@@ -94,12 +87,12 @@
         private bool PlayRound(string correctAnswerMessage)
         {
             _gameAnnouncer.CorrectAnswer(correctAnswerMessage);
-            CurrentPlayer.AddGoldCoin();
-            _gameAnnouncer.PlayerGoldCoins(CurrentPlayer);
+            _gameMaster.GetCurrentPlayer().AddGoldCoin();
+            _gameAnnouncer.PlayerGoldCoins(_gameMaster.GetCurrentPlayer());
 
-            bool winner = CurrentPlayer.DidIWin();
-            _currentPlayerIndex++;
-            StartNewRound();
+            bool winner = _gameMaster.GetCurrentPlayer().DidIWin();
+            _gameMaster.SetNextPlayer();
+            _gameMaster.StartNewRound();
 
             return winner;
         }
@@ -107,21 +100,13 @@
         public bool wrongAnswer()
         {
             _gameAnnouncer.WrongAnswer();
-            _gameAnnouncer.PlayerWasSentToPenaltyBox(CurrentPlayer);
-            CurrentPlayer.InPenaltyBox = true;
+            _gameAnnouncer.PlayerWasSentToPenaltyBox(_gameMaster.GetCurrentPlayer());
+            _gameMaster.GetCurrentPlayer().InPenaltyBox = true;
 
-            _currentPlayerIndex++;
-            StartNewRound();
+            _gameMaster.SetNextPlayer();
+            _gameMaster.StartNewRound();
 
             return true;
-        }
-
-        private void StartNewRound()
-        {
-            if (_currentPlayerIndex == _players.Count)
-            {
-                _currentPlayerIndex = 0;
-            }
         }
     }
 }
---
+++ Kata-RefactoringUglyTrivia/Trivia/Staff/GameMaster.cs
@@ -0,0 +1,49 @@
+﻿namespace Trivia.Staff
+{
+    using System.Collections.Generic;
+
+    public class GameMaster
+    {
+        private readonly GameAnnouncer _gameAnnouncer;
+
+        private readonly List<Player> _players = new List<Player>();
+
+        private int _currentPlayerIndex = 0;
+
+        public GameMaster(GameAnnouncer gameAnnouncer)
+        {
+            _gameAnnouncer = gameAnnouncer;
+        }
+
+        public void AddPlayer(string playerName)
+        {
+            var player = new Player(playerName);
+            _players.Add(player);
+            _gameAnnouncer.PlayerWasAdded(player);
+            _gameAnnouncer.PlayerPosition(_players.Count);
+        }
+
+        public Player GetCurrentPlayer()
+        {
+            return _players[_currentPlayerIndex];
+        }
+
+        public int CountPlayers()
+        {
+            return _players.Count;
+        }
+
+        public void SetNextPlayer()
+        {
+            _currentPlayerIndex++;
+        }
+
+        public void StartNewRound()
+        {
+            if (_currentPlayerIndex == _players.Count)
+            {
+                _currentPlayerIndex = 0;
+            }
+        }
+    }
+}
